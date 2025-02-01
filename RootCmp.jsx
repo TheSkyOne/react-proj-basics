007be6241fe25ec4c908719a523da9a160883552
TheSkyOne <42645782+TheSkyOne@users.noreply.github.com>
2025-02-01 18:48:35 +0200

    added animals list component and its styling

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,12 +1,22 @@
 import { AppHeader } from "./cmps/AppHeader.jsx"
 import { Home } from "./cmps/Home.jsx"
+import { AnimalListCmp } from "./cmps/AnimalListCmp.jsx"
+
+const animalsInfo = [
+    {type: 'Malayan Tiger', count: 787},
+    {type: 'Mountain Gorilla', count: 212},
+    {type: 'Fin Whale', count: 28},
+]
 
 export function RootCmp() {
+
+
     return (
         <section className="app main-layout">
             <AppHeader />
             <main>
                 <Home />
+                <AnimalListCmp animalsInfo={animalsInfo}/>
             </main>
         </section>
     )
---
+++ cmps/AnimalListCmp.jsx
@@ -0,0 +1,22 @@
+export function AnimalListCmp({animalsInfo}){
+    console.log(animalsInfo)
+
+    return (
+        <div className="animals-list">
+            <span>Rare Animals</span>
+            <table>
+                <tbody>
+                    {animalsInfo.map(animal => (
+                    <tr key={animal.type}>
+                        <td>{animal.type}</td>
+                        <td>{animal.count}</td>
+                        <td><a href={`https://www.google.com/search?q=${animal.type}`}>search</a></td>
+                    </tr>))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+
+
